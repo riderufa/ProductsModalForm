e4b4d6039aaeb10f94335f45bfce227a0c8af9ca
Garri <3434455@mail.ru>
2020-09-28 21:17:09 +0500

    delete

--- core/views.py
+++ core/views.py
@@ -16,14 +16,15 @@ def save_products(request):
     products = request.POST.getlist('products[]')[0]
     products = json.loads(products)
     for product in products:
-        date = product['date'] if product['date'] == '' else datetime.date.today()
-        status = product['status'] if product['status'] else 0
-        price_dollar = product['currency_price'] if product['currency_price'] else 0
-        kurs = product['redemption_rate'] if product['redemption_rate'] else 0
-        koef_vikup = product['redemption_ratio'] if product['redemption_ratio'] else 0
-        ves = product['weight'] if product['weight'] else 0
-        price_dostavki = product['send_price'] if product['send_price'] else 0
-        koef_dostavka = product['send_ratio'] if product['send_ratio'] else 0
+        print(product)
+        date = product['date'] if product['date'] != '' else datetime.date.today()
+        status = product['status'] if product['status'] != '' else 0
+        price_dollar = product['currency_price'] if product['currency_price'] != '' else 0
+        kurs = product['redemption_rate'] if product['redemption_rate'] != '' else 0
+        koef_vikup = product['redemption_ratio'] if product['redemption_ratio'] != '' else 0
+        ves = product['weight'] if product['weight'] != '' else 0
+        price_dostavki = product['send_price'] if product['send_price'] != '' else 0
+        koef_dostavka = product['send_ratio'] if product['send_ratio'] != '' else 0
         Product.objects.create(
             date=date,
             brand=product['brand'],
